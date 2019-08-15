06424328edacf5f79509d4a50ebf89e7d7f54aac
Armand McQueen <armandmcqueen@gmail.com>
2019-08-14 23:21:06 -0700

    Quick update

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --network=host -v /mnt/share/ssh:/root/.ssh -it armandmcqueen/sagemaker-ssh:latest /bin/bash
+docker run --network=host -it armandmcqueen/sagemaker-ssh:latest /bin/bash
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Starting ssh daemon"
-/usr/sbin/sshd -p 22; sleep infinity
+/usr/sbin/sshd -p 1234; sleep infinity
