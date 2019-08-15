68c1c49174ef515b1bc8fcbdc2b63742939447da
Armand McQueen <armandmcqueen@gmail.com>
2019-08-15 00:58:36 -0700

    Quick update

--- find_ssh_ip.py
+++ find_ssh_ip.py
@@ -1,9 +1,7 @@
 import boto3
 import json
-import subprocess
+from invoke import task
 
-def sh(cmd):
-    return subprocess.check_output(cmd, shell=True)
 
 
 def filter_by_sgs(network_interfaces, sg_ids):
@@ -48,14 +46,18 @@ def display_network_interfaces(network_interfaces):
         id = n["NetworkInterfaceId"]
         print(id, device_index, status, private_ip, sg_names)
 
-def attempt_ssh(ip):
-    sh(f'ssh root@{ip} cat /opt/ml/input/config/resourceconfig.json')
+
     
 def extract_ips(network_interfaces):
     return [n["PrivateIpAddress"] for n in network_interfaces]
 
 
-if __name__ == '__main__':
+
+
+    
+
+@task()
+def smssh(c):
     subnet = "subnet-21ac2f2e"
     security_groups = ["sg-0eaeb8cc84c955b74",
                        "sg-0043f63c9ad9ffc1d",
@@ -64,10 +66,8 @@ if __name__ == '__main__':
     network_interfaces = filter_by_sgs(network_interfaces, security_groups)
     network_interfaces = filter_by_device_id(network_interfaces, 2)
     display_network_interfaces(network_interfaces)
-    
+
     ips = extract_ips(network_interfaces)
     for ip in ips:
         print(f'Trying ssh to {ip}')
-        attempt_ssh(ip)
-    
-    
+        c.run(f'ssh root@{ip} cat /opt/ml/input/config/resourceconfig.json')
