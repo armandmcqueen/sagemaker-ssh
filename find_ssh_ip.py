1408abe965cd2e733478c85b654bd519a16ccd28
Armand McQueen <armandmcqueen@gmail.com>
2019-08-15 00:55:31 -0700

    Quick update

--- find_ssh_ip.py
+++ find_ssh_ip.py
@@ -31,6 +31,13 @@ def get_network_inferfaces(subnet):
     return response["NetworkInterfaces"]
     # print(json.dumps(response, indent=4))
 
+def filter_by_device_id(network_interfaces, device_id):
+    def device_index_match(network_interface):
+        return network_interface["Attachment"]["DeviceIndex"] == device_id
+
+    matching_network_interfaces = [n for n in network_interfaces if device_index_match(n)]
+    return matching_network_interfaces
+
 
 def display_network_interfaces(network_interfaces):
     for n in network_interfaces:
@@ -42,7 +49,10 @@ def display_network_interfaces(network_interfaces):
         print(id, device_index, status, private_ip, sg_names)
 
 def attempt_ssh(ip):
-    sh(f'ssh root@{ip}')
+    sh(f'ssh root@{ip} cat /opt/ml/input/config/resourceconfig.json')
+    
+def extract_ips(network_interfaces):
+    return [n["PrivateIpAddress"] for n in network_interfaces]
 
 
 if __name__ == '__main__':
@@ -52,4 +62,12 @@ if __name__ == '__main__':
                        "sg-0d931ecdaccd26af3"]
     network_interfaces = get_network_inferfaces(subnet)
     network_interfaces = filter_by_sgs(network_interfaces, security_groups)
+    network_interfaces = filter_by_device_id(network_interfaces, 2)
     display_network_interfaces(network_interfaces)
+    
+    ips = extract_ips(network_interfaces)
+    for ip in ips:
+        print(f'Trying ssh to {ip}')
+        attempt_ssh(ip)
+    
+    
